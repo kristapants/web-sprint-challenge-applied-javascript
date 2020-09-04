8181d2796662fa1e0964afcc24e3a6a6fbf7e57b
kristapants <peachykeen@gmail.com>
2020-09-04 12:26:37 -0500

    finished with MVP

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,57 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+import axios from 'axios'
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then(apiArticleTopics => {
+        Object.keys(apiArticleTopics.data.articles).forEach(articleTopic => {
+            apiArticleTopics.data.articles[articleTopic].forEach(element => {
+                articleMaker(element, articleTopic)
+            })
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+
+const articleBox = document.querySelector('.cards-container')
+
+const articleMaker = ( {authorName, authorPhoto, headline}, topicClass) => {
+    
+    //Create elements
+    const article = document.createElement('div')
+    const headlineDiv = document.createElement('div')
+    const authorBox = document.createElement('div')
+    const authorPicBox = document.createElement('div')
+    const authorPic = document.createElement('img')
+    const authorCredit = document.createElement('span')
+    
+    //Element structure
+    articleBox.appendChild(article)
+    article.appendChild(headlineDiv)
+    article.appendChild(authorBox)
+    authorBox.appendChild(authorPicBox)
+    authorPicBox.appendChild(authorPic)
+    authorBox.appendChild(authorCredit)
+    
+    //Class assignments
+    article.classList.add('card', `topic-${topicClass}`)
+    headlineDiv.classList.add('headline')
+    authorBox.classList.add('author')
+    authorPicBox.classList.add('img-container')
+
+    //Element contents
+    headlineDiv.textContent = headline
+    authorPic.src = authorPhoto
+    authorCredit.textContent = `By ${authorName}`
+
+    article.addEventListener('click', () => {
+        console.log(headline)
+    })
+
+    return article
+}
+
